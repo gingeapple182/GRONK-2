95c45565547b9b3db913a034365f7c2bcaabe7ef
Oliver <129097817+gingeapple182@users.noreply.github.com>
2025-04-11 20:52:59 +0100

    Updated health tracking
    Added in health tracking for player
    Updated enemy to take health instead of kill player
    Reorganised player mechanics

--- gronk-2/Scripts/player.gd
+++ gronk-2/Scripts/player.gd
@@ -1,11 +1,39 @@
 extends CharacterBody2D
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var level_manager: Node = %LevelManager
-@onready var jump: AudioStreamPlayer2D = $Jump
+@onready var jump: AudioStreamPlayer2D = $Audio/Jump
+@onready var die: AudioStreamPlayer2D = $Audio/Die
+@onready var timer: Timer = $Timer
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+var max_health := 100
+var current_health := max_health
+
+# health stuff
+func take_damage(amount: int = 1) -> void:
+	current_health -= amount
+	current_health = max(current_health, 0)
+	die.play() # play sound
+	velocity.y = JUMP_VELOCITY / 1.5 # bounce player on hit
+	print("Player got hit! Current health: ", current_health)
 	
+	if current_health <= 0:
+		player_killed()
+
+func player_killed():
+	print("You died!")
+	collision_shape_2d.queue_free()
+	Engine.time_scale = 0.5
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
+
+# movement stuff
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
---
+++ gronk-2/scripts/enemy.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+@onready var die: AudioStreamPlayer2D = $Die
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.take_damage(25)
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
