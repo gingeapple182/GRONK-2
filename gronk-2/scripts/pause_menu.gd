cc8a0a835d9e80a260cb5f37bb2a91e32bbf9b94
Oliver <129097817+gingeapple182@users.noreply.github.com>
2025-04-14 23:49:39 +0100

    Initial pause menu design

---
+++ gronk-2/scripts/pause_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_button_resume_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_exit_pressed() -> void:
+	pass # Replace with function body.
